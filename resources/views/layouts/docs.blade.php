a6571805be392af591924cebdd9cbeb2f409843c
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-01-25 12:14:31 -0800

    wip

--- resources/views/layouts/docs.blade.php
+++ resources/views/layouts/docs.blade.php
@@ -2,12 +2,33 @@
 
 @section('body')
 
-    <div class="py-2 text-center border-b">
-        <h1>docs.blade.php</h1>
-    </div>
-    
-    <div class="max-w-3xl mx-auto">
-        @yield('content')
-    </div>
-    
+    <div class="flex w-full h-screen">
+        
+        <div class="flex justify-end w-4/12 p-10 topo-bg bg-size-50">
+            <nav class="w-72">
+
+                <!-- Logo Header -->
+                <div class="mb-10">
+                    <a href="{{ route('welcome') }}" class="font-serif text-5xl font-semibold text-yellow-200 hover:text-yellow-200 hover:no-underline">
+                        Oak
+                    </a>
+                </div>
+
+                <ul>
+                    <li class="inverted-links">
+                        <a href="#">Introduction</a>
+                    </li>
+                </ul>
+
+            </nav>
+        </div>
+        
+        <div class="flex-1 flex-grow p-10">
+            <div class="max-w-3xl mx-auto prose prose-lg">
+                @yield('content')
+            </div>
+        </div>
+
+    </div>   
+
 @endsection
